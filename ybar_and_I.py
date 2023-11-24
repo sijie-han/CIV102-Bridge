50f7fbb672496558e4958217d2ec9aa969a2319c
J-Vadakken <jgvadakken5@gmail.com>
2023-11-23 22:57:18 -0500

    Implements a function to calculate Q

--- ybar_and_I.py
+++ ybar_and_I.py
@@ -6,7 +6,7 @@ def y_bar(beam_list):
     # defined as 0 to the top of the beam
     # Where width is the width of the beam
     # Where height is the height of the beam
-    # Outputs y_bar
+    # Outputs y_bar in mm
     y_sum = 0
     a_tot = 0
     for beam in beam_list:
@@ -23,7 +23,7 @@ def I(beam_list):
     # defined as 0
     # Where width is the width of the beam
     # Where height is the height of the beam
-    # Outputs I
+    # Outputs I in mm4
     i_o_sum = 0
     vert_sum = 0
     y_b = y_bar(beam_list)
@@ -32,8 +32,30 @@ def I(beam_list):
         vert_sum += (y_b - beam[0] + beam[2] / 2) ** 2 * beam[1] * beam[2]
     return i_o_sum + vert_sum
 
-def Q(beam_list):
-    pass
+def Q(beam_list, h):
+    # Accepts 
+    # Accepts a 2D list of arguements,
+    # Each element in the list should be of the form:
+    # [y, width, height]
+    # Where y is the vertical distance from an axis 
+    # defined as 0 to the top of the beam
+    # Where width is the width of the beam
+    # Where height is the height of the beam
+    # Outputs Q in mm3
+    aug_b_list = []
+    area = 0
+    for beam in beam_list:
+        if beam[0] < h:
+            aug_b_list.append(beam.copy())
+            area += beam[1]*beam[1]
+        elif beam[0] - beam[2] < h:
+            aug_b_list.append(beam.copy())
+            aug_b_list[-1][0] = h
+            aug_b_list[-1][2] = h - (beam[0] - beam[2])
+            area += aug_b_list[-1][1]*aug_b_list[-1][2]
+
+    d = h - y_bar(aug_b_list)
+    return d * area
 
 if __name__ == "__main__":
     beam_list = [[76.27, 100   ,  1.27],
@@ -47,4 +69,14 @@ if __name__ == "__main__":
     y = y_bar(beam_list)
     print(y)
     print(I(beam_list))
+
+    beam_list = [[241, 241, 241]]
+    print(Q(beam_list, 120.5))
+
+    beam_list = [[100+1.27*2, 120, 1.27],
+                 [100+1.27*1, 120, 1.27],
+                 [100, 20 + 2*1.27, 1.27],
+                 [100-1.27, 1.27, 100-1.27],
+                 [100-1.27, 1.27, 100-1.27],]
+    print(Q(beam_list, y_bar(beam_list)))
     
