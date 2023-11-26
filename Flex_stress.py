f99eb6094a31f3a0b2ff7ecce05ab93c08d6226d
J-Vadakken <jgvadakken5@gmail.com>
2023-11-26 16:22:09 -0500

    Syncing my files

---
+++ Flex_stress.py
@@ -0,0 +1,31 @@
+import Bridge_geometry
+
+def sigma_depth(beam_list, M, dist_from_bottom):
+    I = Bridge_geometry.I(beam_list)
+    h = dist_from_bottom
+    yb = Bridge_geometry.y_bar(beam_list)
+    return M * (h - yb) / I
+
+def sigma_top(beam_list, M):
+    I = Bridge_geometry.I(beam_list)
+    h = Bridge_geometry.y_top(beam_list)
+    yb = Bridge_geometry.y_bar(beam_list)
+    return M * (h - yb) / I
+
+def sigma_bottom(beam_list, M):
+    I = Bridge_geometry.I(beam_list)
+    yb = Bridge_geometry.y_bar(beam_list)
+    return M * yb / I
+
+def tau_cent(beam_list, V):
+    yb = Bridge_geometry.y_bar(beam_list)
+    Q = Bridge_geometry.Q(beam_list, yb)
+    I = Bridge_geometry.I(beam_list)
+    b_c = Bridge_geometry.horizontal_thickness(beam_list, yb)
+    return V * Q / (I * b_c)
+
+def tau_glue(beam_list, V, depth_of_interest, glue_width):
+    h = depth_of_interest
+    Q = Bridge_geometry.Q(beam_list, h)
+    I = Bridge_geometry.I(beam_list)
+    return V * Q / (I * glue_width)
--- Playground1.py
+++ Playground1.py
@@ -1,6 +1,5 @@
 import Bridge_geometry
-import Buckling
-import Stress
+import Flex_stress
 
 
 # Geomtry 0, Design 0
@@ -27,9 +26,9 @@ beam_list = [   [100 + 2*1.27, 120   ,  1.27],
                 [100   ,   1.27, 100  ],
                 ]
 
-I = Bridge_geometry.I(beam_list)
-yb = Bridge_geometry.y_bar(beam_list)
-y_top = Bridge_geometry.y_top(beam_list)
+#I = Bridge_geometry.I(beam_list)
+#yb = Bridge_geometry.y_bar(beam_list)
+#y_top = Bridge_geometry.y_top(beam_list)
 
 # Have to integrate with Ian's code later
 M = 1000 # multiplied by ____ factor p
@@ -37,65 +36,58 @@ V = 546 # multuplied by ____ factor p
 
 # Unironically really hard to do the rest of the steps without a good BMD and SFD
 
-print(I, yb)
+#print(I, yb)
 
 
-# Geometry 1 Bentz design
+# Geometry 2 Square
 
 
 #            <100>
-#     |‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|   | 1.27
-#     |_________________|   | 1.27      *One layers at top
-#     |  |___|   |__|  |    | 1.27     | 
-#     |  |<10>  <10>|  |               |
-#     |  |          |  |               | 100
-#     |  |          |  |               |
-#     |  |          |  |               |
-#     |__|__________|__|               |
-#     |________________|
-#    <1.27>         <1.27>
+#    |‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|   | 1.27        |
+#    |                  |   | 1.27        |
+#    |__________________|   | 1.27        | *three layers at top
+#     |  |___|   |__|  |    | 1.27  |     | 
+#     |  |<15>  <15>|  |    |       |     |
+#     |  |          |  |    |       |     | 100
+#     |  |          |  |    |       |     |
+#     |  |<15>  <15>|  |    |       |94.92|
+#     |  |___    ___|  |    |       |     |
+#     |  |___|__|___|  |    |       |     |
+#     |__|__________|__|    | 1.27
+#    <1.27>  <80>   <1.27>
+#            
 
 
-beam_list = [   [100 + 1.27, 100   ,  1.27],
-                [100   ,   10   ,  1.27],
-                [100   ,   10   ,  1.27],
-                [100   ,   1.27, 100-1.27],
-                [100   ,   1.27, 100-1.27],
-                [1.27   ,   1.27, 100  ],
+beam_list = [   [100, 100   ,  1.27],
+                [100 - 1.27, 100   ,  1.27],
+                [100 - 2 * 1.27, 100,  1.27],
+                [100 - 3*1.27,   15   ,  1.27],
+                [100 - 3*1.27,   15   ,  1.27],
+                [100  - 3*1.27,   1.27, 100-4*1.27],
+                [100  - 3*1.27,   1.27, 100-4*1.27],
+                [1.27*2 , 15, 1.27],
+                [1.27*2 , 15, 1.27],
+                [1.27   ,   80, 1.27]
                 ]
 
 I = Bridge_geometry.I(beam_list)
 yb = Bridge_geometry.y_bar(beam_list)
 y_top = Bridge_geometry.y_top(beam_list)
-M_max = 83851.323
-V = 307.145
+M_max = 0.0782 * 10 ** 6 #[Nmm] times factor P / 446.667 where P is the heaviest load 
+V = 296.48 #[N] times factor P / 446.667 where P is the heaviest load 
 
-# Calculating max stresses.
-sigma_compression = Stress.sigma_top(beam_list, M_max)
-print(f"sigma_compression = {sigma_compression}")
-sigma_tension = Stress.sigma_bottom(beam_list, M_max)
-print(f"sigma_tension = {sigma_tension}")
-tau_g = Stress.tau_glue(beam_list, V, 98.73, 20+2*1.27)  # Convert kN to N
-print(f"tau_glue = {tau_g}")
-tau_max = Stress.tau_cent(beam_list, V)  # Convert kN to N
-print(f"tau_max = {tau_max}")
+# I and y_bar
+print(I)
+print(yb)
 
+# flexurial stresses.
 
-FOS_tension = 30 / sigma_tension
-print(f"FOS_tension = {FOS_tension}")
-FOS_compression = 6 / sigma_compression
-print(f"FOS_compression = {FOS_compression}")
-FOS_shear = 4 / tau_max
-print(f"FOS_shear = {FOS_shear}")
-FOS_glue = 2 / tau_g
-print(f"FOS_glue = {FOS_glue}")
+stress_top = Flex_stress.sigma_top(beam_list, M_max)
+stress_bot = Flex_stress.sigma_bottom(beam_list, M_max)
+tau_cent = Flex_stress.tau_cent(beam_list, V)
 
-# Buckling
-FOS_shear_buckling = tau_cr4() / tau_max
-print(f"FOS_shear_buckling = {FOS_shear_buckling}")
-FOS_flexural_buckling1 = sigma_cr1() / sigma_compression
-print(f"FOS_flexural_buckling1 = {FOS_flexural_buckling1}")
-FOS_flexural_buckling2 = sigma_cr2() / sigma_compression
-print(f"FOS_flexural_buckling2 = {FOS_flexural_buckling2}")
-FOS_flexural_buckling3 = sigma_cr3(beam_list) / sigma_compression
-print(f"FOS_flexural_buckling3 = {FOS_flexural_buckling3}")
+print("In tension:")
+print(stress_top, stress_bot)
+print("strain:")
+print(tau_cent)
+
