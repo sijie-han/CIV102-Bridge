04b58f15762863d4d02fd0932453f57317c10dbb
J-Vadakken <jgvadakken5@gmail.com>
2023-11-26 04:51:11 -0500

    1 Iteration done, although it's probably not good

--- Playground1.py
+++ Playground1.py
@@ -1,5 +1,6 @@
 import Bridge_geometry
 import Buckling
+import Stress
 
 
 # Geomtry 0, Design 0
@@ -38,3 +39,63 @@ V = 546 # multuplied by ____ factor p
 
 print(I, yb)
 
+
+# Geometry 1 Bentz design
+
+
+#            <100>
+#     |‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾|   | 1.27
+#     |_________________|   | 1.27      *One layers at top
+#     |  |___|   |__|  |    | 1.27     | 
+#     |  |<10>  <10>|  |               |
+#     |  |          |  |               | 100
+#     |  |          |  |               |
+#     |  |          |  |               |
+#     |__|__________|__|               |
+#     |________________|
+#    <1.27>         <1.27>
+
+
+beam_list = [   [100 + 1.27, 100   ,  1.27],
+                [100   ,   10   ,  1.27],
+                [100   ,   10   ,  1.27],
+                [100   ,   1.27, 100-1.27],
+                [100   ,   1.27, 100-1.27],
+                [1.27   ,   1.27, 100  ],
+                ]
+
+I = Bridge_geometry.I(beam_list)
+yb = Bridge_geometry.y_bar(beam_list)
+y_top = Bridge_geometry.y_top(beam_list)
+M_max = 83851.323
+V = 307.145
+
+# Calculating max stresses.
+sigma_compression = Stress.sigma_top(beam_list, M_max)
+print(f"sigma_compression = {sigma_compression}")
+sigma_tension = Stress.sigma_bottom(beam_list, M_max)
+print(f"sigma_tension = {sigma_tension}")
+tau_g = Stress.tau_glue(beam_list, V, 98.73, 20+2*1.27)  # Convert kN to N
+print(f"tau_glue = {tau_g}")
+tau_max = Stress.tau_cent(beam_list, V)  # Convert kN to N
+print(f"tau_max = {tau_max}")
+
+
+FOS_tension = 30 / sigma_tension
+print(f"FOS_tension = {FOS_tension}")
+FOS_compression = 6 / sigma_compression
+print(f"FOS_compression = {FOS_compression}")
+FOS_shear = 4 / tau_max
+print(f"FOS_shear = {FOS_shear}")
+FOS_glue = 2 / tau_g
+print(f"FOS_glue = {FOS_glue}")
+
+# Buckling
+FOS_shear_buckling = tau_cr4() / tau_max
+print(f"FOS_shear_buckling = {FOS_shear_buckling}")
+FOS_flexural_buckling1 = sigma_cr1() / sigma_compression
+print(f"FOS_flexural_buckling1 = {FOS_flexural_buckling1}")
+FOS_flexural_buckling2 = sigma_cr2() / sigma_compression
+print(f"FOS_flexural_buckling2 = {FOS_flexural_buckling2}")
+FOS_flexural_buckling3 = sigma_cr3(beam_list) / sigma_compression
+print(f"FOS_flexural_buckling3 = {FOS_flexural_buckling3}")
